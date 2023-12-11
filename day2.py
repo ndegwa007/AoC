0042b0c69fc59de65f0d120e4d0d3f6485db84b1
ndegz <ndegwa8781@gmail.com>
2023-12-11 14:46:57 +0300

    day2 complete

--- day2.py
+++ day2.py
@@ -24,6 +24,26 @@ The Elf would first like to know which games would have been possible if the bag
 In the example above, games 1, 2, and 5 would have been possible if the bag had been loaded with that configuration. However, game 3 would have been impossible because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been impossible because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get 8.
 
 Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+--PART TWO--
+The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+
+As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color that could have been in the bag to make the game possible?
+
+Again consider the example games from earlier:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+
+For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
 """
 
 import re
@@ -40,6 +60,9 @@ def game():
     p_red = re.compile(r'\d+ (?=red)')
     p_green = re.compile(r'\d+ (?=green)')
 
+    # list with max color elements in each game
+    product_list = []
+
     # read the file
 
     with open('game1.txt', 'r') as file:
@@ -54,12 +77,19 @@ def game():
 
             if len(b) == len(lis_blue) and len(lis_green) == len(g) and len(r) == len(lis_red):
                 result.extend(p.findall(line))
+            # part two
+            product = max([int(x) for x in r]) * max([int(y) for y in g]) * max([int(z) for z in b])
+            product_list.append(product)
+
     # sum of the game ids
     total = 0
     for id in result:
         total += int(id)
+    #return total
 
-    return total
+    # product sum of each game for the fewest choices per color
+    return sum(product_list)
+
 
 if __name__ == '__main__':
     print(game())
